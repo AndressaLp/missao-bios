fa8fc0d2abb83cf454584402063fb39368d25bb3
AndressaLp <andressarodrigueslopes157@gmail.com>
2026-09-18 09:16:17 -0300

    feat: implementar verificacao de conclusao do laboratorio

--- scripts/Puzzle/LaboratorioPuzzle.cs
+++ scripts/Puzzle/LaboratorioPuzzle.cs
@@ -5,8 +5,35 @@ using System.Threading.Tasks;
 
 namespace MissaoBios.scripts.Puzzle
 {
-    public class LaboratorioPuzzle
+    // Junta todas as amostras do Laboratório e verifica quando o puzzle inteiro foi resolvido.
+    public partial class LaboratorioPuzzle : Node
     {
-        
+        [Export] public NodePath[] CaminhosAmostras;
+
+        private List<AmostraController> _amostras = new();
+        private int _totalAmostras = 3;
+
+        public override void _Ready()
+        {
+            foreach (var caminho in CaminhosAmostras)
+            {
+                _amostras.Add(GetNode<AmostraController>(caminho));
+            }
+        }
+
+        public bool PuzzleCompleto()
+        {
+            int resolvidas = 0;
+
+            foreach (var amostra in _amostras)
+            {
+                if (!amostra.EstaResolvida())
+                {
+                    resolvidas++;
+                }
+            }
+
+            return resolvidas >= _totalAmostras;
+        }
     }
 }
